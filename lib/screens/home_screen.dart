37a51bd442bfe9064a4dbdd62b10c6271cfaa215
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2021-12-30 16:56:55 -0700

    homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,10 +5,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Widgets'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: const Text('ruta'),
+                  leading: const Icon(Icons.access_alarm),
+                  onTap: () {},
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: 10));
   }
 }
