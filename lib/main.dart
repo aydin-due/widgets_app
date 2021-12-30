db7bca901c4583deab7aa850bdbbcfb2b91769fa
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2021-12-30 16:11:38 -0700

    ListView builder & separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_app/screens/listview1_screen.dart';
+//import 'package:widgets_app/screens/listview1_screen.dart';
+import 'package:widgets_app/screens/listview2_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,6 +12,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: Listview1Screen());
+        home: Listview2Screen());
   }
 }
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const ['ama', 'me', 'mie'];
+
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView 2'),
+        ),
+        body: ListView.separated(
+          // separated has separatorBuiler & builder hasn't
+          itemCount: options.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(options[index]),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          separatorBuilder: (_, __) =>
+              const Divider(), //se usa _ cuando no se usan los args
+        ));
+  }
+}
