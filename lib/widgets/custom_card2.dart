4aa3d8cf940a8f5231c44c2eed8b6ab824f582d6
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-06-05 16:34:24 -0700

    img & card adjustments

--- lib/widgets/custom_card2.dart
+++ lib/widgets/custom_card2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/theme/appTheme.dart';
 
 class CustomCard2 extends StatelessWidget {
   const CustomCard2({Key? key}) : super(key: key);
@@ -6,9 +7,14 @@ class CustomCard2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+        clipBehavior:
+            Clip.antiAlias, //widget hijo imita la apariencia del widget padre
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15)), //borde de la card
+        shadowColor: AppTheme.primary.withOpacity(0.5),
         elevation: 10,
         child: Column(
-          children: const [
+          children: [
             // imagen normal
             /*
             Image(
@@ -16,10 +22,19 @@ class CustomCard2 extends StatelessWidget {
                     'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Sunset_Sky_Wallpaper_%285080737981%29.jpg/1200px-Sunset_Sky_Wallpaper_%285080737981%29.jpg'))
           '*/
             // muestra un repuesto en lo q carga la imagen
-            FadeInImage(
-                placeholder: AssetImage('assets/jar-loading.gif'),
-                image: NetworkImage(
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Sunset_Sky_Wallpaper_%285080737981%29.jpg/1200px-Sunset_Sky_Wallpaper_%285080737981%29.jpg'))
+            const FadeInImage(
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              image: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Sunset_Sky_Wallpaper_%285080737981%29.jpg/1200px-Sunset_Sky_Wallpaper_%285080737981%29.jpg'),
+              width: double.infinity, //tomar todo el ancho posible
+              height: 230,
+              fit: BoxFit.cover,
+              fadeInDuration: Duration(milliseconds: 300),
+            ),
+            Container(
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                child: const Text('Un hermoso paisaje'))
           ],
         ));
   }
