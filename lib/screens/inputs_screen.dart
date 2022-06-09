fb2ad1308cc0a226e7e569f52a7cfedb036c97bb
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-06-08 22:45:57 -0700

    form widget

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,6 +7,16 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'aydin',
+      'last_name': 'salman',
+      'email': 'aydin@si.com',
+      'password': 'si',
+      'role': 'admin'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs & '),
@@ -15,20 +25,53 @@ class InputsScreen extends StatelessWidget {
         //como listview pero con 1 solo hijo
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(children: const [
-            // TextField() //no necesita q lo agrupe 1 form
-            CustomInputField(labelText: 'Nombre', hintText: 'Nombre del usuario'),
-            SizedBox(height: 30,),
-            CustomInputField(labelText: 'Apellido', hintText: 'Apellido del usuario'),
-            SizedBox(height: 30,),
-            CustomInputField(labelText: 'Correo', hintText: 'Correo del usuario', keyboardType: TextInputType.emailAddress,),
-            SizedBox(height: 30,),
-            CustomInputField(labelText: 'Contraseña', hintText: 'Contraseña del usuario', obscureText: true,),
-            SizedBox(height: 30,),
-          ]),
+          child: Form(
+            key: myFormKey,
+            child: Column(children: [
+              // TextField() //no necesita q lo agrupe 1 form
+              const CustomInputField(
+                  labelText: 'Nombre', hintText: 'Nombre del usuario'),
+              const SizedBox(
+                height: 30,
+              ),
+              const CustomInputField(
+                  labelText: 'Apellido', hintText: 'Apellido del usuario'),
+              const SizedBox(
+                height: 30,
+              ),
+              const CustomInputField(
+                labelText: 'Correo',
+                hintText: 'Correo del usuario',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const CustomInputField(
+                labelText: 'Contraseña',
+                hintText: 'Contraseña del usuario',
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+
+              ElevatedButton(
+                  child: const SizedBox(
+                      width: double.infinity,
+                      child: Center(child: Text('Guardar'))),
+                  onPressed: () {
+                    if (!myFormKey.currentState!.validate()) {
+                      // ! en current state es para decir q no va a ser null nunk
+                      print('formulario no válido');
+                      return;
+                    }
+                    print(formValues);
+                  })
+            ]),
+          ),
         ),
       ),
     );
   }
 }
-
