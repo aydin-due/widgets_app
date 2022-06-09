d7f95d05fc056a1e522cbedd63ae2e11405add64
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-06-08 21:11:06 -0700

    personalized text form fields

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -20,9 +20,32 @@ class InputsScreen extends StatelessWidget {
               autofocus: true,
               initialValue: 'si',
               textCapitalization: TextCapitalization.words,
-              onChanged: (value) { //cada q se cambia algún caracter, se ejecuta la función
+              onChanged: (value) {
+                //cada q se cambia algún caracter, se ejecuta la función
                 print('value: $value');
               },
+              validator: (value) {
+                if (value == null) return 'Este campo es requerido';
+                return value.length < 3 ? 'Mínimo de 3 letras' : null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction, //muestra el return del validator
+              decoration: const InputDecoration(
+                hintText: 'Nombre del usuario',
+                labelText: 'Nombre',
+                helperText: 'Sólo letras',
+                counterText: '3 caracteres',
+                suffixIcon: Icon(Icons.text_fields_sharp),
+                prefixIcon: Icon(Icons.supervised_user_circle),
+                icon: Icon(Icons.access_alarms),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green)
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    topRight: Radius.circular(10))
+                )
+              ),
             )
           ]),
         ),
