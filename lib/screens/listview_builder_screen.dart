5ab8ff173586fae8f188b6d9552107fd119ea4d3
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-06-14 02:40:27 -0700

    infinite scroll w/ futures

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/theme/appTheme.dart';
 
 class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
@@ -10,16 +11,30 @@ class ListViewBuilderScreen extends StatefulWidget {
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
   final List<int> imageID = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
     scrollController.addListener(() {
       // print('${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
-      if ((scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent) {
-        add5();
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        // add5();
+        fetchData();
       }
     });
+  }
+
+  Future fetchData() async {
+    //espera mientras tiene los resultados
+    if (isLoading) return; //para q no se ejecute cada q se mueve la pantalla
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 3));
+    add5();
+    isLoading = false;
+    setState(() {});
   }
 
   void add5() {
@@ -31,28 +46,58 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: MediaQuery.removePadding(
         //quita los espacios vacíos
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          //carga imágenes de manera perezosa  (cargan mientras scrolleas)
-          physics:
-              const BouncingScrollPhysics(), //efecto al final d la pantalla d ios
-          controller: scrollController,
-          itemCount: imageID.length,
-          itemBuilder: (BuildContext context, int index) {
-            return FadeInImage(
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-                placeholder: const AssetImage('assets/jar-loading.gif'),
-                image: NetworkImage(
-                    'https://picsum.photos/500/300?image=${imageID[index]}'));
-          },
+        child: Stack(
+          children: [
+            ListView.builder(
+              //carga imágenes de manera perezosa  (cargan mientras scrolleas)
+              physics:
+                  const BouncingScrollPhysics(), //efecto al final d la pantalla d ios
+              controller: scrollController,
+              itemCount: imageID.length,
+              itemBuilder: (BuildContext context, int index) {
+                return FadeInImage(
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                    placeholder: const AssetImage('assets/jar-loading.gif'),
+                    image: NetworkImage(
+                        'https://picsum.photos/500/300?image=${imageID[index]}'));
+              },
+            ),
+            Positioned(
+              bottom: 40, 
+              left: size.width*0.5-30, 
+              child: const _LoadingIcon())
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _LoadingIcon extends StatelessWidget {
+  const _LoadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.9), shape: BoxShape.circle),
+      child: const CircularProgressIndicator(
+        color: AppTheme.primary,
       ),
     );
   }
